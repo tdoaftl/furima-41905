96acce56ea10b66d4a58621ea13768880be190d6
tdoaftl <twelvedaysoff.and.fivetealatte@gmail.com>
2024-12-26 20:19:05 +0900

    金額表示実装

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,19 +1,21 @@
 function price (){
 
   const sellPrice  = document.getElementById("item-price");
-  sellPrice.addEventListener("keyup", () => {
-    const sellPriceValue = sellPrice.value; // 入力値を取得し数値に変換
-    const profit = sellPriceValue * 0.9; // 計算を実行
-    const fee = sellPriceValue * 0.8;
+  sellPrice.addEventListener("input", () => {
+    const sellPriceValue = sellPrice.value; 
+    
+    const fee = Math.floor(sellPriceValue * 0.1);
+    const profit = sellPriceValue - fee;
 
 
-    const feeValue  = document.getElementById("add-tax-price");  //要素
-    feeValue.innerHTML = `${fee}`;
+    const feeValue  = document.getElementById("add-tax-price"); 
+    feeValue.innerHTML = `${fee.toLocaleString()}`;
 
-    const profitValue  = document.getElementById("profit");  //要素
-    profitValue.innerHTML = `${profit}`;
+    const profitValue  = document.getElementById("profit"); 
+    profitValue.innerHTML = `${profit.toLocaleString()}`;
 
   });
 };
 
 window.addEventListener('turbo:load', price);
+window.addEventListener("turbo:render", price);
