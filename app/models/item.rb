0e121091a92f1645d0bb29831737adf65b1052a2
tdoaftl <twelvedaysoff.and.fivetealatte@gmail.com>
2024-12-26 16:04:13 +0900

    ActiveHash設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,20 @@
 class Item < ApplicationRecord
   has_one_attached :image
 
-  validates :name,            presence: true                    
-  validates :explain,         presence: true                       
-  validates :category_id,     presence: true                    
-  validates :condition_id,    presence: true                    
-  validates :shipping_id,     presence: true                    
-  validates :prefecture_id,   presence: true                    
-  validates :day_id,          presence: true                    
-  validates :price,           presence: true   
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping
+  belongs_to :day
+  belongs_to :prefecture
+
+  validates :name,            presence: true
+  validates :explain,         presence: true
+  validates :category_id,     numericality: { other_than: 1 } 
+  validates :condition_id,    presence: true
+  validates :shipping_id,     presence: true
+  validates :prefecture_id,   presence: true
+  validates :day_id,          presence: true
+  validates :price,           presence: true
+
 end
