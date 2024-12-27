01ffee1ca6f2de79d21a512b210cb088078e9ca8
tdoaftl <twelvedaysoff.and.fivetealatte@gmail.com>
2024-12-27 19:53:52 +0900

    詳細表示ページ実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,19 +5,21 @@ class ItemsController < ApplicationController
     @items = Item.includes(:user).order("created_at DESC")
   end
 
-
   def new
     @item = Item.new
   end
 
   def create
-
     @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
    else
      render :new, status: :unprocessable_entity
    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
 
