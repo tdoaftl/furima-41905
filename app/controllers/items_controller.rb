57ad31bfb8482ac35d366c0f1f8626ec7ca323f8
tdoaftl <twelvedaysoff.and.fivetealatte@gmail.com>
2024-12-27 21:32:10 +0900

    商品編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: :edit
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -22,10 +23,30 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if  @item.update(item_params)
+     redirect_to item_path
+   else
+     render :edit, status: :unprocessable_entity
+   end
+ end
+
 
   private
 
   def item_params
     params.require(:item).permit(:name, :explain, :image, :price,:category_id,:condition_id,:shipping_id,:prefecture_id,:day_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to action: :index
+    end
   end
 end
