c7f5bed53f972c9bff91f3e7805776af6d720007
tdoaftl <twelvedaysoff.and.fivetealatte@gmail.com>
2024-12-28 14:23:04 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :move_to_index]
   before_action :move_to_index, only: :edit
 
@@ -34,6 +34,12 @@ class ItemsController < ApplicationController
      render :edit, status: :unprocessable_entity
    end
  end
+
+ def destroy
+  item = Item.find(params[:id])
+  item.destroy
+  redirect_to root_path 
+end
 
 
   private
