a924d80fe27e93d96bf236724b4c9787e15bad04
tdoaftl <twelvedaysoff.and.fivetealatte@gmail.com>
2024-12-28 19:06:14 +0900

    空欄バリデーション

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -3,4 +3,11 @@ class Delivery < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
+
+  validates :image,           presence: true
+  validates :postcode,        presence: true
+  validates :prefecture_id,   presence: true
+  validates :city,            presence: true
+  validates :address,         presence: true
+  validates :phonenumber,     presence: true
 end
