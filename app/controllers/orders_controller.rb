53d4c7c333e325f0598d66d4b5a37fb9f9217583
tdoaftl <twelvedaysoff.and.fivetealatte@gmail.com>
2024-12-29 22:19:17 +0900

    カード機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,20 +1,25 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: :index
+  before_action :set_item, only: [:index, :create, :move_to_root_path]
   before_action :move_to_root_path, only: :index
 
 
   def index 
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+    
     @buy_delivery = BuyDelivery.new
   end
 
   def create
+    params[:buy_delivery][:price] = @item.price
+
     @buy_delivery = BuyDelivery.new(buy_params)
-    
      if @buy_delivery.valid?
+         pay_item
         @buy_delivery.save
-       redirect_to root_path
+        redirect_to root_path
       else
-        @item = Item.find(params[:item_id])
+        gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
         render :index, status: :unprocessable_entity
       end 
   end
@@ -23,13 +28,25 @@ class OrdersController < ApplicationController
     private
     
     def buy_params
-        params.require(:buy_delivery).permit(:postcode, :prefecture_id, :city, :address, :building, :phonenumber).merge(item_id: params[:item_id], user_id: current_user.id)
+        params.require(:buy_delivery).permit(:postcode, :prefecture_id, :city, :address,:building, :phonenumber, :price).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
     end
 
-    def move_to_root_path
-        @item = Item.find(params[:item_id])
+    def move_to_root_path  
       if (current_user.id == @item.user_id) || (Buy.exists?(item_id: @item.id))
          redirect_to root_path
       end
+    end
+
+    def set_item
+      @item = Item.find(params[:item_id])
+    end
+
+    def pay_item
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: buy_params[:price],
+        card: buy_params[:token], 
+        currency: 'jpy'
+      )
     end
 end
--- app/models/buy_delivery.rb
+++ app/models/buy_delivery.rb
@@ -1,16 +1,17 @@
 class BuyDelivery
   include ActiveModel::Model
 
-  attr_accessor :postcode, :prefecture_id, :city, :address, :building, :phonenumber, :item_id, :user_id
+  attr_accessor :postcode, :prefecture_id, :city, :address, :building, :phonenumber, :item_id, :user_id, :price, :token
 
   with_options presence: true do
+    validates :token
     validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows(e.g. 123-4567)"}
     validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :city
     validates :address
     validates :phonenumber, length: { minimum: 10,maximum: 11, message: 'is too short'}
     validates :user_id
-    validates :item_id 
+    validates :item_id
   end
   validates :phonenumber, numericality: { only_integer: true, message: 'is invalid. Input only number'}
   
